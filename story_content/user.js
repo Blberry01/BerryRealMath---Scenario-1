f87b06522d76f18045207d19e39d6780a42a7b2a
Blberry01 <berryhelpfulco@gmail.com>
2024-09-24 11:25:22 -0600

    BerryRealMath---Scenario 1

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6CYCm8mleV0":
+        Script1();
+        break;
   }
 }
 
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var min = 1;
+var max = 10;
+var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+player.SetVar("randomNum", randomNum);
+
+}
+
+};
